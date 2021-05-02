d917b9c3e2a95d3fee4b75877d4fb5d224e5fd36
JoseRoncal2002 <u201912909@upc.edu.pe>
2021-05-01 22:56:57 -0500

    create-player-endpoint

---
+++ HelpI.API/Domain/Persistence/Repositories/IPlayerRepository.cs
@@ -0,0 +1,17 @@
+﻿using HelpI.API.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpI.API.Domain.Persistence.Repositories
+{
+    public interface IPlayerRepository
+    {
+        Task<IEnumerable<Player>> ListAsync();
+        Task AddAsync(Player player);
+        Task<Player> FindById(int id);
+        void Update(Player player);
+        void Remove(Player player);
+    }
+}
---
+++ HelpI.API/Persistence/Repositories/PlayerRepository.cs
@@ -0,0 +1,44 @@
+﻿using HelpI.API.Domain.Models;
+using HelpI.API.Domain.Persistence.Contexts;
+using HelpI.API.Domain.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpI.API.Persistence.Repositories
+{
+    public class PlayerRepository : BaseRepository, IPlayerRepository
+    {
+        public PlayerRepository(AppDbContext context) : base(context)
+        {
+
+        }
+
+        public async Task AddAsync(Player player)
+        {
+            await _context.Players.AddAsync(player);
+        }
+
+        public async Task<Player> FindById(int id)
+        {
+            return await _context.Players.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Player>> ListAsync()
+        {
+            return await _context.Players.ToListAsync();
+        }
+
+        public void Remove(Player player)
+        {
+            _context.Players.Remove(player);
+        }
+
+        public void Update(Player player)
+        {
+            _context.Players.Update(player);
+        }
+    }
+}
