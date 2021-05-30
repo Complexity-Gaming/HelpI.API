e9c134e6b1cc79954a6d2f51caec0f4dadabda94
DiegoJohnsonL <diegojohnson46@gmail.com>
2021-05-30 17:29:08 -0500

    Value Objects accessibility change

--- HelpI.API/Domain/Models/Training/TrainingMaterial.cs
+++ HelpI.API/Domain/Models/Training/TrainingMaterial.cs
@@ -9,8 +9,8 @@ namespace HelpI.API.Domain.Models.Training
     public class TrainingMaterial
     {
         public int Id { get; set; }
-        public TrainingId TrainingMaterialId;
-        public TrainingDetail TrainingDetails { get; set; }
+        public TrainingId TrainingMaterialId { get; }
+        public TrainingDetail TrainingDetails { get; }
         public int ExpertId { get; set; }
         public Expert CreatedBy { get; set; }
         public List<PlayerTrainingMaterial> PlayerTrainingMaterials { get; set; }
