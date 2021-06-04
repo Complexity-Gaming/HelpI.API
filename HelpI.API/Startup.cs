7abe2549818a13915af26361e3e14ef2fce28f31
DiegoJohnsonL <diegojohnson46@gmail.com>
2021-06-03 21:51:00 -0500

    Update env.IsProduction()

--- HelpI.API/Startup.cs
+++ HelpI.API/Startup.cs
@@ -90,12 +90,13 @@ namespace HelpI.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HelpI.API v1"));
             }
+            
 
             app.UseHttpsRedirection();
 
